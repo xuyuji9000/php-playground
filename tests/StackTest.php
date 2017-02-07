dd2ae43f3e3c3871d2fcd64f2cb2cccbb5fdac5a
yogiman <xuyuji9000@163.com>
2017-02-07 17:59:47 +0800

    PHPUnit test dependency

--- tests/StackTest.php
+++ tests/StackTest.php
@@ -4,6 +4,25 @@ use PHPUnit\Framework\TestCase;
 
 class StackTest extends TestCase
 {
+    public function testEmpty() 
+    {
+        $stack = [];
+        $this->assertEmpty($stack);
+
+        return $stack;
+    }
+
+    /**
+     * @depends testEmpty
+     */
+    public function testPush(array $stack)
+    {
+        array_push($stack, 'Mary');
+        $this->assertEquals(1, count($stack));
+        $this->assertEquals('Mary', $stack[count($stack)-1]);
+        return $stack;
+    }
+/*
     public function testPushAndPop() 
     {
         $stack = [];
@@ -15,4 +34,5 @@ class StackTest extends TestCase
         $this->assertEquals('Mary', array_pop($stack));
         $this->assertEquals(0, count($stack));
     }
+*/
 }
